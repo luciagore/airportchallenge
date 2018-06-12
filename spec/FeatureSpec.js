453f946fd614f0aa6a927b08be4d9090113957d8
luciagore <lucy.gore@hotmail.co.uk>
2018-06-12 20:45:06 +0100

    second user story: clear planes for takeoff

--- spec/FeatureSpec.js
+++ spec/FeatureSpec.js
@@ -3,6 +3,11 @@
 // I want to instruct a plane to land at
 //   an airport and confirm that it has landed
 
+// As an air traffic controller
+// To get passengers to a destination
+// I want to instruct a plane to take off from
+//   an airport and confirm that it is no longer in the airport
+
 'use strict';
 
 describe('Feature Test', function() {
@@ -17,5 +22,11 @@ describe('Feature Test', function() {
   it('planes can be instructed to land at an airport', function() {
     plane.land(airport);
     expect(airport.planes()).toContain(plane);
+  });
+
+  it('planes can be instructed to takeoff', function() {
+    plane.land(airport);
+    plane.takeoff();
+    expect(airport.planes()).not.toContain(plane);
   });
 });
