5dbf9fdbf7a1dfbc77d2c6c4160349dc590085a7
luciagore <lucy.gore@hotmail.co.uk>
2018-06-13 12:17:11 +0100

    preventing planes from landing in stormy weather

--- src/Airport.js
+++ src/Airport.js
@@ -5,6 +5,9 @@ function Airport(){
 }
 Airport.prototype.planes = function(){ return this._hangar; };
 Airport.prototype.clearForLanding = function(plane) {
+  if(this.isStormy()) {
+    throw new Error('cannot takeoff during storm');
+  }
   this._hangar.push(plane);
 };
 Airport.prototype.clearForTakeOff = function(plane) {
